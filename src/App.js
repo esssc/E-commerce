2bb6a3ce31b509ac27ed30f2f57dc2ac707343e4
esssc <37302254+esssc@users.noreply.github.com>
2020-04-11 17:11:59 +0100

    puting routes to page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
-function App() {
-  return (
+const Hats = () => (
     <div>
-      <HomePage/>
+        HAts page
     </div>
-  );
+);
+
+function App() {
+    return (
+        <div>
+            <Switch>
+                <Route exact path="/" component={HomePage}/>
+                <Route  exact path="/shop/hats" component={Hats}/>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
