95b2721cc28a75a0c883ea2accc23b096d54881d
esssc <37302254+esssc@users.noreply.github.com>
2020-04-28 11:23:51 +0100

    added checkout page, checkout-item.component.js and updated cart to use selectors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import {Switch, Route, Redirect } from 'react-router-dom';
+import { createStructuredSelector } from "reselect";
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
+
 import SignInAndSignUpPage from "./pages/sing-in-and-sing-up/sing-in-and-sing-up.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { connect } from 'react-redux';
+import { selectCurrentUser } from "./redux/user/user.selectors";
 
 import { setCurrentUser } from "./redux/user/user.actions";
 
@@ -49,6 +53,7 @@ class App extends React.Component {
                 <Switch>
                     <Route exact path="/" component={HomePage}/>
                     <Route  path="/shop" component={ShopPage}/>
+                    <Route exact path="/checkout" component={CheckoutPage} />
                     <Route exact  path="/signin" render= {() => this.props.currentUser ? (<Redirect to="/"/>) : (<SignInAndSignUpPage/>)}/>
                 </Switch>
             </div>
@@ -56,8 +61,8 @@ class App extends React.Component {
     }
 }
 
-const mapStateToProps = ({ user }) => ({
-    currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser
 });
 
 const mapDispatchToProps = dispatch => ({
--- src/components/collection-item/collection-item.component.js
+++ src/components/collection-item/collection-item.component.js
@@ -17,7 +17,7 @@ const CollectionItem = ({  item, addItem }) => (
 
         <div className="collection-footer">
             <span className="name">{item.name}</span>
-            <span className="price">{item.price}</span>
+            <span className="price">{item.price}£</span>
         </div>
         <CustomButton inverted onClick={() => addItem(item)} >Add to cart</CustomButton>
     </div>
